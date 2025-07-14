3c52e09773b56130fd191808f4c2367dff42e327
tomoayan <63101155+tomoayan@users.noreply.github.com>
2025-07-14 18:34:40 +0530

    improved UI

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let isLogin = true
 let activeKeys = []
-let shortcuts = {}
-let keys = {}
+let keyDomList = {}
+// let shortcuts = {}
+
+let keyboardSelector = document.getElementById('keyboard-selector');
+
 
 addEventListener("load", async () => {
-
-
-
-
     setTimeout(() => {
         document.addEventListener('keydown', function (event) {
             const key = event.key;
@@ -15,78 +14,49 @@ addEventListener("load", async () => {
                 if (!activeKeys.includes(key)) {
                     // Key not active, so activate it!
                     activeKeys.push(key);
-                    keys[key].classList.add("active");
+                    keyDomList[key].classList.add("active");
                 } else {
                     const keyIndex = activeKeys.indexOf(key);
                     activeKeys.splice(keyIndex, 1);
-                    keys[key].classList.remove("active");
+                    keyDomList[key].classList.remove("active");
                 }
             } catch (e) {
-                const keyList = Object.keys(keys)
+                const keyList = Object.keys(keyDomList)
                 if (keyList.includes(e.key)) {
-                    console.warn(e.key + " key is availble the the virtual keyboard list, error is somewhere else")
+                    console.warn(key + " - key is availble the the virtual keyboard list, error is somewhere else")
                 } else {
-                    console.error(e.key + " key is not availble on the virtual keyboard. Please report this on github issues")
-                    console.log(e)
+                    console.error(key + " - key is not availble on the virtual keyboard. Please report this on github issues")
                 }
+                console.log(e)
             }
         });
     }, 500);
 
-
-
-
-    try {
-
-        const tomoElementExtractRegex = /<tomo-element>(?<element>.*)<\/tomo-element>.*?style>(?<style>.*)<\/style>/s;
-        const keyboardList = ['/keyboard/generic.html']
-
-        const res = await fetch(keyboardList[0]).then(res => res.text());
-        let element = tomoElementExtractRegex.exec(res)
-
-        const keyboard_wrapper = document.getElementById('keyboard-wrapper')
-
-        keyboard_wrapper.innerHTML = element.groups.element + '<style>' + element.groups.style + '</style>';
-
-
-        const keyboard = document.querySelectorAll('.keyboard .row > div')
-        for (const key of keyboard) {
-            // key.dataset.keyPrimary === acKey || key.dataset.keySecondary === acKey
-            if (key.dataset.keyPrimary) {
-                keys[key.dataset.keyPrimary] = key;
-            }
-
-            if (key.dataset.keySecondary) {
-                keys[key.dataset.keyPrimary] = key;
-            }
-        }
-    } catch (error) {
-        console.error(error);
-    }
-
+    keyboardSelector.addEventListener('change', keyboard_load())
 })
 
 
 
 
 
-// async function getData() {
-//     const url = "/saved.json";
-//     try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//             throw new Error(`Response status: ${response.status}`);
-//         }
+const keyboard_load = async () => {
+    const tomoElementExtractRegex = /<tomo-element>(?<element>.*)<\/tomo-element>.*?style>(?<style>.*)<\/style>/s;
+    const selectedKeyboard = keyboardSelector.value
 
-//         const json = await response.json();
-//         shortcuts = json
-//         // console.log(Object.keys(json));
-//     } catch (error) {
-//         console.error(error.message);
-//     }
-// };
-// getData();
+    let res;
+    try {
+        res = await fetch(selectedKeyboard).then(res => res.text());
+    } catch (err) {
+        console.warn('error while fetching keyboard code')
+    }
 
+    let extractedCode = tomoElementExtractRegex.exec(res)
+    const keyboard_wrapper = document.getElementById('keyboard-wrapper')
+    keyboard_wrapper.innerHTML = extractedCode.groups.element + '<style>' + extractedCode.groups.style + '</style>';
 
-
-
+    const keyboard = document.querySelectorAll('.keyboard .row > div')
+    for (const key of keyboard) {
+        if (key.dataset.keyPrimary) keyDomList[key.dataset.keyPrimary] = key;
+        if (key.dataset.keySecondary) keyDomList[key.dataset.keySecondary] = key;
+    }
+}
