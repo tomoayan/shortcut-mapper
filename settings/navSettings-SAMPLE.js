8a172626c5bc3f0fd5a4f7592fa2e879932ea5f5
tomoayan <63101155+tomoayan@users.noreply.github.com>
2025-08-11 20:30:57 +0530

    chaos: my brain logic error

--- settings/navSettings-SAMPLE.js
+++ settings/navSettings-SAMPLE.js
@@ -1,33 +1,35 @@
-export const navSettings = {
+export const activeNavOptionsSAMPLE = {
     heading: "",
     description: "",
-    section: {
-        subHeading: "",
-        tooltip: "",
-        items: [
-            {
-                type: 'toggle',
-                name: "",
-                callback: `callback fn ref`,
-                abortSignal: `AbortControllerSignal ref`
-            },
-            {
-                type: 'slider',
-                name: "",
-                callback: `callback fn ref`,
-                abortSignal: `AbortControllerSignal ref`
-            },
-            {
-                type: 'select',
-                options: [
-                    {
-                        name: "",
-                        value: "",
-                    }
-                ],
-                callback: `callback fn ref`,
-                abortSignal: `AbortControllerSignal ref`
-            },
-        ]
-    }
+    section: [
+        {
+            subHeading: "",
+            tooltip: "",
+            items: [
+                {
+                    type: 'toggle',
+                    name: "",
+                    callback: `callback fn ref`,
+                    abortSignal: `AbortControllerSignal ref`
+                },
+                {
+                    type: 'slider',
+                    name: "",
+                    callback: `callback fn ref`,
+                    abortSignal: `AbortControllerSignal ref`
+                },
+                {
+                    type: 'select',
+                    options: [
+                        {
+                            name: "",
+                            value: "",
+                        }
+                    ],
+                    callback: `callback fn ref`,
+                    abortSignal: `AbortControllerSignal ref`
+                },
+            ]
+        }
+    ]
 }
