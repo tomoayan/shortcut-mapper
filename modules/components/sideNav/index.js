0e28f244581b49c6b21325ee88a9fa3b42a3b247
tomoayan <63101155+tomoayan@users.noreply.github.com>
2026-03-09 01:44:22 +0530

    typo

--- modules/components/sideNav/index.js
+++ modules/components/sideNav/index.js
@@ -40,7 +40,7 @@ export const sidebar = () => {
                         <path d="M8 12h.01" />
                         <rect width="20" height="16" x="2" y="4" rx="2" />
                     </svg>`,
-                desc: `Select your keyboard layout to improve viulization`,
+                desc: `Select your keyboard layout to improve visualization.`,
                 bodyDOM: optionItemKeyboardBody()
             },
         ])
