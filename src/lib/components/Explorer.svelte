819d7a33e92618c36b66e993b672565453f81084
tomoayan <63101155+tomoayan@users.noreply.github.com>
2026-06-09 10:13:36 +0000

    recovered options-wrapper (shortcut menu)

--- src/lib/components/Explorer.svelte
+++ src/lib/components/Explorer.svelte
@@ -83,10 +83,25 @@
                             <span>{shortcut.shortcut.split('⌨').join(' + ')}</span>
                         </div>
                     </div>
-                    <!-- svelte-ignore a11y_click_events_have_key_events -->
-                    <!-- svelte-ignore a11y_no_static_element_interactions -->
-                    <div class="delete-btn" on:click={() => deleteShortcut(shortcut.id)}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 11v6" /><path d="M14 11v6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" /><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+                    <div class="options-wrapper">
+                        <span title="Pin">
+                            <span></span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-icon lucide-pin"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>
+                        </span>
+                        <span title="Edit">
+                            <span></span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
+                        </span>
+                        <span title="Move">
+                            <span></span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-symlink-icon lucide-folder-symlink"><path d="M2 9.35V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H20a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h7"/><path d="m8 16 3-3-3-3"/></svg>
+                        </span>
+                        <!-- svelte-ignore a11y_click_events_have_key_events -->
+                        <!-- svelte-ignore a11y_no_static_element_interactions -->
+                        <span class="remove-icon" title="Delete" on:click={() => deleteShortcut(shortcut.id)}>
+                            <span></span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+                        </span>
                     </div>
                 </div>
                 {#if shortcut.extrainfo}
@@ -204,6 +219,7 @@ ul.shortcut-list li {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    overflow: hidden;
 }
 
 .title {
@@ -234,17 +250,78 @@ p.extra-info {
     font-weight: 400;
 }
 
-.delete-btn {
-    opacity: 0.3;
-    cursor: pointer;
-    transition: opacity .2s;
-    padding: .5rem;
+div.options-wrapper {
+    transform: translateX(calc(100% + 1.3rem));
+    opacity: .3;
+    transition: transform .4s cubic-bezier(1, 0, 1, 1) 0s, opacity .4s cubic-bezier(1, 0, 1, 1) 0s;
+    position: relative;
+    height: 2rem;
+    width: 10rem;
+    display: flex;
 }
-.delete-btn:hover {
+
+ul.shortcut-list li:hover div.options-wrapper {
+    transform: translateX(0);
     opacity: 1;
+    transition: transform .4s cubic-bezier(.3, 0, 0, .9) 0.5s, opacity .4s cubic-bezier(.3, 0, 0, .9) 0.5s;
 }
-.delete-btn svg {
-    width: 1.2rem;
-    height: 1.2rem;
+
+div.options-wrapper > span {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    display: block;
+    cursor: pointer;
+    transition: .2s ease;
+    transform: translateY(-50%);
+    padding: .5rem;
+    transition-property: padding, background-color;
+}
+
+div.options-wrapper > span svg {
+    width: 1.05rem;
+    height: 1.05rem;
+    color: #b3b3b3;
+    transition: scale .2s ease;
+}
+
+div.options-wrapper > span:active svg {
+    scale: .9;
+}
+
+div.options-wrapper > span > span {
+    position: absolute;
+    inset: 0;
+    scale: 0;
+    border-radius: .5rem;
+    transition: .2s ease;
+    transition-property: scale, background-color;
+}
+
+div.options-wrapper > span:hover > span {
+    scale: 1;
+    background-color: hsla(0, 0%, 100%, 0.2);
+}
+
+div.options-wrapper > span:nth-child(1) {
+    translate: -6.6rem 0;
+    pointer-events: none;
+    opacity: .5;
+}
+
+div.options-wrapper > span:nth-child(2) {
+    translate: -4.4rem 0;
+    pointer-events: none;
+    opacity: .5;
+}
+
+div.options-wrapper > span:nth-child(3) {
+    translate: -2.2rem 0;
+    pointer-events: none;
+    opacity: .5;
+}
+
+div.options-wrapper > span:nth-child(4) {
+    translate: 0rem 0;
 }
 </style>
